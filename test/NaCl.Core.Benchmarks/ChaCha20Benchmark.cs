b3d9aa6dcf161a12d057bc308d327e01cf98a5e4
Timothy Makkison <timmakkison@gmail.com>
2022-10-08 23:10:25 +0100

    Alter Dencrypt test

--- test/NaCl.Core.Benchmarks/ChaCha20Benchmark.cs
+++ test/NaCl.Core.Benchmarks/ChaCha20Benchmark.cs
@@ -2,10 +2,8 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
 
     using Base;
-    using Internal;
 
     using BenchmarkDotNet.Attributes;
 
@@ -19,6 +17,7 @@
         private Memory<byte> key;
         private Memory<byte> nonce;
         private Memory<byte> message;
+        private Memory<byte> cipherText;
         private ChaCha20 cipher;
 
         [Params(
@@ -42,6 +41,10 @@
             message = new byte[Size];
             rnd.NextBytes(message.Span);
 
+            cipherText = new byte[Size];
+            var c = new ChaCha20(key, 0);
+            c.Encrypt(message.Span, nonce.Span, cipherText.Span);
+
             cipher = new ChaCha20(key, 0);
         }
 
@@ -55,32 +58,35 @@
 
         [Benchmark]
         [BenchmarkCategory("Decryption")]
-        [ArgumentsSource(nameof(TestVectors))]
-        public void Decrypt(Tests.Vectors.Rfc8439TestVector test)
+        public void Decrypt()
         {
-            var plaintext = new byte[test.CipherText.Length];
-            var cipher = new ChaCha20(test.Key, test.InitialCounter);
-            cipher.Decrypt(test.CipherText, test.Nonce, plaintext);
+            var plaintext = new byte[cipherText.Length];
+            cipher.Decrypt(cipherText.Span, nonce.Span, plaintext);
         }
 
-        public IEnumerable<object> TestVectors()
-        {
-            //foreach (var test in Tests.Rfc8439TestVector.Rfc8439TestVectors)
-            //    yield return test;
-
-            yield return Tests.Vectors.Rfc8439TestVector.Rfc8439TestVectors[0];
-            yield return Tests.Vectors.Rfc8439TestVector.Rfc8439TestVectors[1];
-            yield return Tests.Vectors.Rfc8439TestVector.Rfc8439TestVectors[2];
-            yield return Tests.Vectors.Rfc8439TestVector.Rfc8439TestVectors[3];
-            yield return Tests.Vectors.Rfc8439TestVector.Rfc8439TestVectors[4];
-            yield return Tests.Vectors.Rfc8439TestVector.Rfc8439TestVectors[5];
-            yield return Tests.Vectors.Rfc8439TestVector.Rfc8439TestVectors[6];
-            yield return Tests.Vectors.Rfc8439TestVector.Rfc8439TestVectors[7];
-        }
-
-        // TODO: Use the encrypt value (from Encrypt method) to benchmark decryption
         //[Benchmark]
         //[BenchmarkCategory("Decryption")]
-        //public byte[] Decrypt(byte[] ciphertext) => cipher.Decrypt(ciphertext);
+        //[ArgumentsSource(nameof(TestVectors))]
+        //public void Decrypt(Tests.Vectors.Rfc8439TestVector test)
+        //{
+        //    var plaintext = new byte[test.CipherText.Length];
+        //    var cipher = new ChaCha20(test.Key, test.InitialCounter);
+        //    cipher.Decrypt(test.CipherText, test.Nonce, plaintext);
+        //}
+
+        //public IEnumerable<object> TestVectors()
+        //{
+        //    //foreach (var test in Tests.Rfc8439TestVector.Rfc8439TestVectors)
+        //    //    yield return test;
+
+        //    yield return Tests.Vectors.Rfc8439TestVector.Rfc8439TestVectors[0];
+        //    yield return Tests.Vectors.Rfc8439TestVector.Rfc8439TestVectors[1];
+        //    yield return Tests.Vectors.Rfc8439TestVector.Rfc8439TestVectors[2];
+        //    yield return Tests.Vectors.Rfc8439TestVector.Rfc8439TestVectors[3];
+        //    yield return Tests.Vectors.Rfc8439TestVector.Rfc8439TestVectors[4];
+        //    yield return Tests.Vectors.Rfc8439TestVector.Rfc8439TestVectors[5];
+        //    yield return Tests.Vectors.Rfc8439TestVector.Rfc8439TestVectors[6];
+        //    yield return Tests.Vectors.Rfc8439TestVector.Rfc8439TestVectors[7];
+        //}
     }
 }
